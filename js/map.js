d2cf1d76f4866e38bb245cde07712dda4ad893dd
Ygor <ygor@Ygor-Inspiron-N4110.(none)>
2014-05-04 22:41:50 -0300

    funciona ¬¬

--- js/map.js
+++ js/map.js
@@ -6,7 +6,7 @@ var map;
 
 function initialize() {
     var mapOptions = {
-        zoom: 6
+        zoom: 15
     };
     map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
